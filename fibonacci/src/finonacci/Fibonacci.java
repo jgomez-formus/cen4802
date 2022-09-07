06691d17b068f1b5245acaa2eea6b1220eff4756
Juan Gomez <juan.gomez@thetraxcompany.com>
2022-09-06 23:48:09 -0400

    Fix issue 2 includes java doc

--- fibonacci/src/finonacci/Fibonacci.java
+++ fibonacci/src/finonacci/Fibonacci.java
@@ -1,8 +1,21 @@
+/**
+ * Fibonacci Sequence Analysis
+ * Fibonacci sequence - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+ *
+ */
+
 
 package finonacci;
 
 public class Fibonacci {
 
+    /**
+     * Recursive method that return a fibonacci number based on sequence inputNumber
+     *
+     * @author Juan Gomez Blandon
+     * @param inputNumber integer giving the position of the fibonacci number
+     * @return
+     */
     public static int fibonacciRecursion(int inputNumber) {
         // return specific fibonacci number for input numbers 0 and 1
         if (inputNumber == 0) {
